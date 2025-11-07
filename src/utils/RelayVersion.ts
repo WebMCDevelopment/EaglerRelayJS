85a509b6e9b1069a3464f9505190d8765f6fc467
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2025-11-07 08:14:38 -0600

    init

---
+++ src/utils/RelayVersion.ts
@@ -0,0 +1,7 @@
+export class RelayVersion {
+  public static readonly NAME = 'OpenEaglerRelay';
+  public static readonly VERSION = '0.1.0';
+  public static readonly BRAND = 'colbster';
+  public static readonly PROTOCOL = 1;
+  public static readonly DEFAULT_COMMENT = `${RelayVersion.NAME} v${RelayVersion.VERSION}`;
+}
